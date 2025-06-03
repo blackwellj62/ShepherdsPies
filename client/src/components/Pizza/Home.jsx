8b22a2116a1e2d8f6d09c8d8d195586bac3db1de
Joey Blackwell <blackwellj6241@gmail.com>
2025-06-03 15:43:15 -0500

    added home

--- client/src/components/Pizza/Home.jsx
+++ client/src/components/Pizza/Home.jsx
@@ -1,18 +1,26 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { getAllOrders } from "../../managers/orderManager.js"
 
 export const Home = () => {
     const [orders, setOrders] = useState([])
+
+    useEffect(()=>{
+        getAllOrders().then(orderArray=>{
+            setOrders(orderArray)
+        })
+    },[])
 
     return(
         <div>
             <h2>All Orders</h2>
             {orders.map((order)=>
-                <div class="card" style="width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">Order #{order.id}</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                        <a href="#" class="card-link">Card link</a>
-                        <a href="#" class="card-link">Another link</a>
+                <div key={order.id} className="card" style={{width: "18rem,"}}>
+                    <div className="card-body">
+                        <h5 className="card-title">Order #{order.id}</h5>
+                        <h6 className="card-subtitle mb-2 text-body-secondary">Order Date: {order.orderDateTime}</h6>
+                        <p className="card-text"># of Pizzas: {order.pizzas.length}</p>
+                        <a href={order.id} className="card-link">Order Details</a>
+                        <a href={order.id} className="card-link">Update Order</a>
                     </div>
                 </div>
             )}
